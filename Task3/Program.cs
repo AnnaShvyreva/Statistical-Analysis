4539a4dc6094e354a1c5cce1cb7a06091a12d43c
AnnaShvyreva <nuta_autumn@mail.ru>
2014-11-06 23:40:42 +0400

    Частота встречаеминских букв.

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -13,13 +13,57 @@ namespace Task3
             string s_rus = "ычжчйбч гзшзиб лвщъывчйбх лзйвызкйвов бгвфызтчйбх пвгквтйв люъчк лыбпцчичйбх йвпчмжбе швщъбтчйбм п вфцзщъб увклэяъчыйне ъчейвцвобм.п ызгцбийне щъызйзе кбыз лывпвшхъщх бщщцчшвпзйбх лв щвгшзйбя ъчейвцвобм б зцовыбъквп щжбпзйбх кйвтчщъпз бгвфызтчйбм. щ лвквсэя ызгцбийне зцовыбъквп бсюъ пвгквтйвщъэ ющувыбъэ щжбпзйбч лзйвызк, йч ъчыхх лыб ьъвк п ызгкчызе бгвфызтчйбх, чов вфсчм ьущлвгбабб б ызгйввфызгйне бщузтчйбхе лыб лывачшюыч щжбпзйбх.чщцб щшчцзъэ йчщувцэув щйбкувп щ вшйвм  ъвиуб б лывщъв ызгкчщъбъэ бе ыхшвк п двъвычшзуъвыч, ычгюцэъзъ вузгнпзчъщх йч юшвпцчъпвыбъчцэйнк, лвъвкю иъв фюшюъ пбшйн кчщъз щвчшбйчйбх въшчцэйне изщъчм щжбпзчквм лзйвызкн. лвцюибъэ узшын, увъвынч фн бшчзцэйв щвщънувпнпзцбщэ шыюо щ шыюовк, лызуъбичщуб йчычзцэйв, пчшэ йз бъвовпнм ычгюцэъзъ пцбхяъ ызгцбийнч дзуъвын, ъзубч узу: бгкчйчйбч вщпчсчйбх, бгкчйчйбч двующйвов ызщщъвхйбх вфрчуъбпз. двъвоызд квтчъ увйъывцбывпзъэ ъчейбую, йв йч лыбывшю. вичйэ изщъв лыб щрчкуч лзйвызк лвхпцхяъщх йчтчцзъчцэйнч фцбуб въ пвшйвм бцб гчыузцэйвм лвпчыейвщъб. лвьъвкю бщлвцэгюяъщх щлчабзцэйнч юъбцбън шцх щжбпзйбх двъвщйбкувп п лзйвызкйнч бгвфызтчйбх.щвпычкчййнч лывоызккйнч лывшюуън лвгпвцхяъ щвгшзпзъэ воывкйнч лзйвызкн, щвщъвхсбч въ йчщувцэубе шв йчщувцэубе щвъчй щйбкувп. шцх щвгшзйбх лзйвызкн щ пнщвувм шчъзцбгзабчм ъычфючъщх евывжзх влъбуз йз зллзызъч шцх лывпчшчйбх щрчкуб, з ъзу тч квсйзх пнибщцбъчцэйзх щъзйабх. лыб щвгшзйбб бгвфызтчйбм фвцэжвов ызгычжчйбх ъычфюяъщх воывкйнч квсйвщъб въ пнибщцбъчцэйвм щъзйабб, йз увъвывм лывбгпвшбъщх щфвыуз. щ ьъвм ачцэя ызгызфзънпзяъщх зцовыбъкн ющувычйбх щфвыуб.лвщувцэую пв пычкх щрчкуб ычобщъыбыючъщх кйвтчщъпв шзййне, щ ызгцбийнк лвцвтчйбчк п лывщъызйщъпч, пщч лывоызккн бщлвцэгюяъ зцовыбъкн пнщъызбпзйбх бгвфызтчйбм п йютйвк лвыхшуч. гзъчк щцчшючъ лвщъывчйбч щчъуб бг увйъывцэйне ъвичу. вщйвпйзх лывфцчкз лыб пнщъызбпзйбб бгвфызтчйбм п «йзцвтчйбб» узшывп шыюо йз шыюоз. ичк щбцэйчч йзуцзшнпзчъщх узшы, ъчк кчйэжч пчывхъйвщъэ вжбфуб лыб лвщъывчйбб щчъуб увйъывцэйне ъвичу.йвпчмжбк ьъзлвк ызгпбъбх щбщъчк щвгшзйбх лзйвызкйне щйбкувп хпцхяъщх щбщъчкн ычобщъызабб бгвфызтчйбх щ йчщувцэубе узкчы. ъзу тч лвцюибцв ызщлывщъызйчйбч бщлвцэгвпзйбч жбывувюовцэйне вфрчуъбпвп. ьъв лвгпвцхчъ щвуызъбъэ увцбичщъпв двъвщйбкувп, йв юпчцбибпзчъ лывфцчкю бщузтчйбм б еывкзъбичщубе зфчыызабм. ";
             string s_eng = "isq wnxqvcqc dyt pyci cydifhtq htq uqcnzvqu iy ihbq hfhj jyat dtqquyp iy cshtq hvu xshvzq ni.  lj xyvithci, isq zva zqvqthw ealwnx wnxqvcq nc nviqvuqu iy zahthviqq jyat dtqquyp iy cshtq hvu xshvzq dtqq cydifhtq--iy phbq catq isq cydifhtq nc dtqq dyt hww nic acqtc.  isnc zqvqthw ealwnx wnxqvcq heewnqc iy pyci yd isq dtqq cydifhtq dyavuhinyv'c cydifhtq hvu iy hvj yisqt etyzthp fsycq haisytc xyppni iy acnvz ni.  (cypq yisqt dtqq cydifhtq dyavuhinyv cydifhtq nc xykqtqu lj isq zva wnlthtj zqvqthw ealwnx wnxqvcq nvciqhu.)  jya xhv heewj ni iy jyat etyzthpc, iyy.   fsqv fq ceqhb yd dtqq cydifhtq, fq htq tqdqttnvz iy dtqquyp, vyi etnxq.  yat zqvqthw ealwnx wnxqvcqc htq uqcnzvqu iy phbq catq ishi jya shkq isq dtqquyp iy uncitnlaiq xyenqc yd dtqq cydifhtq (hvu xshtzq dyt isnc cqtknxq nd jya fncs), ishi jya tqxqnkq cyatxq xyuq yt xhv zqi ni nd jya fhvi ni, ishi jya xhv xshvzq isq cydifhtq yt acq enqxqc yd ni nv vqf dtqq etyzthpc; hvu ishi jya bvyf jya xhv uy isqcq isnvzc.   iy etyiqxi jyat tnzsic, fq vqqu iy phbq tqcitnxinyvc ishi dytlnu hvjyvq iy uqvj jya isqcq tnzsic yt iy hcb jya iy cattqvuqt isq tnzsic. isqcq tqcitnxinyvc ithvcwhiq iy xqtihnv tqceyvcnlnwninqc dyt jya nd jya uncitnlaiq xyenqc yd isq cydifhtq, yt nd jya pyundj ni.   dyt qrhpewq, nd jya uncitnlaiq xyenqc yd caxs h etyzthp, fsqisqt zthinc yt dyt h dqq, jya paci znkq isq tqxnenqvic hww isq tnzsic ishi jya shkq.  jya paci phbq catq ishi isqj, iyy, tqxqnkq yt xhv zqi isq cyatxq xyuq.  hvu jya paci csyf isqp isqcq iqtpc cy isqj bvyf isqnt tnzsic.   fq etyiqxi jyat tnzsic fnis ify ciqec: (1) xyejtnzsi isq cydifhtq, hvu (2) yddqt jya isnc wnxqvcq fsnxs znkqc jya wqzhw eqtpnccnyv iy xyej, uncitnlaiq hvu/yt pyundj isq cydifhtq.   hwcy, dyt qhxs haisyt'c etyiqxinyv hvu yatc, fq fhvi iy phbq xqtihnv ishi qkqtjyvq avuqtcihvuc ishi isqtq nc vy fhtthvij dyt isnc dtqq cydifhtq.  nd isq cydifhtq nc pyundnqu lj cypqyvq qwcq hvu ehccqu yv, fq fhvi nic tqxnenqvic iy bvyf ishi fshi isqj shkq nc vyi isq ytnznvhw, cy ishi hvj etylwqpc nvityuaxqu lj yisqtc fnww vyi tqdwqxi yv isq ytnznvhw haisytc' tqeaihinyvc.  dnvhwwj, hvj dtqq etyzthp nc istqhiqvqu xyvcihviwj lj cydifhtq ehiqvic.  fq fncs iy hkynu isq uhvzqt ishi tquncitnlaiytc yd h dtqq etyzthp fnww nvunknuahwwj ylihnv ehiqvi wnxqvcqc, nv qddqxi phbnvz isq etyzthp etyetnqihtj.  iy etqkqvi isnc, fq shkq phuq ni xwqht ishi hvj ehiqvi paci lq wnxqvcqu dyt qkqtjyvq'c dtqq acq yt vyi wnxqvcqu hi hww.   isq etqxncq iqtpc hvu xyvuninyvc dyt xyejnvz, uncitnlainyv hvu pyundnxhinyv dywwyf.     zva zqvqthw ealwnx wnxqvcq    iqtpc hvu xyvuninyvc dyt xyejnvz, uncitnlainyv hvu pyundnxhinyv   0. isnc wnxqvcq heewnqc iy hvj etyzthp yt yisqt fytb fsnxs xyvihnvc h vyinxq ewhxqu lj isq xyejtnzsi sywuqt chjnvz ni phj lq uncitnlaiqu avuqt isq iqtpc yd isnc zqvqthw ealwnx wnxqvcq.  isq \"etyzthp\", lqwyf, tqdqtc iy hvj caxs etyzthp yt fytb, hvu h \"fytb lhcqu yv isq etyzthp\" pqhvc qnisqt isq etyzthp yt hvj uqtnkhinkq fytb avuqt xyejtnzsi whf: ishi nc iy chj, h fytb xyvihnvnvz isq etyzthp yt h eytinyv yd ni, qnisqt kqtlhinp yt fnis pyundnxhinyvc hvu/yt ithvcwhiqu nviy hvyisqt whvzahzq.  (sqtqnvhdiqt, ithvcwhinyv nc nvxwauqu fnisyai wnpnihinyv nv isq iqtp \"pyundnxhinyv\".)  qhxs wnxqvcqq nc huutqccqu hc \"jya\".";
 
+            var stat_eng = new double[26] {0.0817, 0.0149, 0.0278, 0.0425, 0.127, 0.0223, 0.0202, 0.0609, 0.0697, 0.0015, 0.0077, 0.0403, 0.0241, 0.0675, 0.0751, 0.0193, 0.001, 0.0599, 0.0633, 0.0906, 0.0276, 0.0098, 0.0236, 0.0015, 0.0197, 0.0007};
+            var sort_stat_eng = new double[26] { 0.0817, 0.0149, 0.0278, 0.0425, 0.127, 0.0223, 0.0202, 0.0609, 0.0697, 0.0015, 0.0077, 0.0403, 0.0241, 0.0675, 0.0751, 0.0193, 0.001, 0.0599, 0.0633, 0.0906, 0.0276, 0.0098, 0.0236, 0.0015, 0.0197, 0.0007 };
+            Array.Sort(sort_stat_eng);
+
             var freq_rus = FreqRUS(s_rus);
+            var sort_rus = FreqRUS(s_rus);
+            Array.Sort(sort_rus);
+
+            s_rus = s_rus.ToUpper();
+            string s_res = "";
+            for (int i = 0; i < s_rus.Length; i++)
+            {
+                if (s_rus[i]!=' ')
+                {
+                    var b = s_rus[i];
+                    var num = freq_rus[(int)((char)b - 'А')];
+                    int k = 0;
+                    for (int jj = 0; jj < sort_rus.Length; jj++)
+                    {
+                        if (sort_rus[jj] == num) k = jj;
+                    }
+
+                    for (int j = 0; j < stat_eng.Length; j++)
+                    {
+                        if (k < stat_eng.Length)
+                            if (stat_eng[j] == sort_stat_eng[k])
+                            {
+                                s_res = s_res + (char)(j + 'A');
+                                break;
+                            }
+                        /*for (int m = i; m < s_rus.Length; m++)
+                        {
+                            if (s_rus[m] == b)
+                            {
+                                s_rus[m] = (char)(j + 'A');
+                            }
+                        }*/
+                    }
+                }
+            }
+
+            Console.WriteLine(s_res);
+            /*
             for (int i = 0; i < freq_rus.Length; i++)
                 Console.Write("{0} - {1:0.0000}\t", (char)(i + 'А'), freq_rus[i]);
             Console.WriteLine();
-            Console.WriteLine();
+            Console.WriteLine();*/
 
             var freq_eng = FreqENG(s_eng);
+            var sort_eng = FreqENG(s_eng);
+            Array.Sort(sort_eng);
             for (int i = 0; i < freq_eng.Length; i++)
                 Console.Write("{0} - {1:0.0000}\t", (char)(i + 'A'), freq_eng[i]);
             Console.WriteLine();
